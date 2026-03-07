3003601780d27bd6c7624340190fb1c78c0a8892
marsil1990 <ma.silveram@gmail.com>
2026-03-07 11:03:54 -0300

    marcos--team2

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,37 @@
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
+  constructor(productId, dataSource) {
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+  }
+  async init() {
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+    document
+      .getElementById("addToCart")
+      .addEventListener("click", this.addProductToCart.bind(this));
+  }
+  addProductToCart() {
+    const cart = getLocalStorage("so-cart") || [];
+    cart.push(this.product);
+    setLocalStorage("so-cart", cart);
+  }
+  renderProductDetails() {
+    const productDetail = document.querySelector(".product-detail");
+
+    productDetail.querySelector("h3").textContent = this.product.Brand.Name;
+    productDetail.querySelector("h2").textContent =
+      this.product.NameWithoutBrand;
+    productDetail.querySelector("img").src = this.product.Image;
+    productDetail.querySelector("img").alt = this.product.Name;
+    productDetail.querySelector(".product-card__price").textContent =
+      `$${this.product.FinalPrice}`;
+    productDetail.querySelector(".product__color").textContent =
+      this.product.Colors[0].ColorName;
+    productDetail.querySelector(".product__description").innerHTML =
+      this.product.DescriptionHtmlSimple;
+    productDetail.querySelector("#addToCart").dataset.id = this.product.Id;
+  }
+}
