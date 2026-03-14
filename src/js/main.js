60bb225cc6ecc7b2f7a72fefc6c3fb355e7d0dae
marsil1990 <ma.silveram@gmail.com>
2026-03-14 16:59:31 -0300

    team3

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,12 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { cartCount } from "./utils.mjs";
+import { cartCount, loadHeaderFooter } from "./utils.mjs";
 
 //My MAIN
 
 const productData = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
 const productList = new ProductList("tents", productData, listElement);
+loadHeaderFooter();
 productList.init();
 cartCount();
--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,12 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { cartCount } from "./utils.mjs";
+import { cartCount, loadHeaderFooter } from "./utils.mjs";
 //Product
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
 const product = new ProductDetails(productId, dataSource);
+loadHeaderFooter();
 product.init();
 cartCount();
