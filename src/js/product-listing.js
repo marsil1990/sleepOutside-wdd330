11d2e133075e376f90c10dbbd2e0e03eed6a7b31
EmiRivero2003 <eriveronovo@byupathway.edu>
2026-03-20 01:44:00 -0300

    Add product search functionality

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -2,19 +2,35 @@ import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import { cartCount, loadHeaderFooter, getParam } from "./utils.mjs";
 
-//My MAIN
+// My MAIN
 
 const category = getParam("category");
+const search = getParam("search");
+const query = search || category;
+
 const productData = new ProductData();
 const listElement = document.querySelector(".product-list");
-const productList = new ProductList(category, productData, listElement);
-loadHeaderFooter();
-productList.init();
-cartCount();
+const productList = new ProductList(query, productData, listElement);
 
-const maxInput = document.getElementById("maxPrice");
-const maxButton = document.getElementById("filterBtn");
-maxButton.addEventListener("click", () => {
-  const max = Number(maxInput.value);
-  productList.sortListByprice(max);
-});
+async function initPage() {
+  loadHeaderFooter();
+
+  if (search) {
+    const results = await productData.searchProducts(search);
+    productList.renderList(results);
+  } else {
+    productList.init();
+  }
+
+  cartCount();
+
+  const maxInput = document.getElementById("maxPrice");
+  const maxButton = document.getElementById("filterBtn");
+
+  maxButton.addEventListener("click", () => {
+    const max = Number(maxInput.value);
+    productList.sortListByprice(max);
+  });
+}
+
+initPage();
